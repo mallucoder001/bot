7917f49a93f370b6ad1c3296f62ec4597fbdd3c4
mallucoder001 <brcchekuthan@gmail.com>
2024-05-19 15:28:44 +0530

    Add files via upload

--- m.py
+++ m.py
@@ -235,8 +235,8 @@ def handle_bgmi(message):
             target = command[1]
             port = int(command[2])  # Convert time to integer
             time = int(command[3])  # Convert port to integer
-            if time > 181:
-                response = "Error: Time interval must be less than 80."
+            if time > 600:
+                response = "Error: Time interval must be less than 600."
             else:
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
@@ -283,7 +283,7 @@ def show_help(message):
 💥 /plan : Checkout Our Botnet Rates.
 
 🤖 To See Admin Commands:
-💥 /admincmd : Shows All Admin Commands.
+💥 /admincmd : Shows All Admin Commands.'''
 
 
     for handler in bot.message_handlers:
